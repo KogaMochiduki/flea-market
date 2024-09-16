59afbb646bf6d6fe0a890bbb9c031ced9b28ca69
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2024-09-16 21:16:07 +0900

    add Jest

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    // moduleDirectories: ['node_modules', '<rootDir>/src'],
+    moduleDirectories: ['node_modules', '<rootDir>/src'],
     testEnvironment: 'jsdom',
 }
 module.exports = createJestConfig(customJestConfig)
