6cc5e65ac6976a6b919cae6528da0acae83e9835
KogaMochiduki <b1646038@planet.kanazawa-it.ac.jp>
2024-09-22 22:05:13 +0900

    add: Badge component

---
+++ src/components/atoms/Badge/index.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Badge from './index'
+
+const meta: Meta<typeof Badge> = {
+  title: 'Atoms/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  argTypes: {
+    content: {
+      control: { type: 'text' },
+      description: 'バッジのテキスト',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    backgroundColor: {
+      control: { type: 'color' },
+      description: 'バッジの色',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Orange: Story = {
+  args: {
+    content: '1',
+    backgroundColor: '#ed9f28',
+  },
+}
+
+export const Green: Story = {
+  args: {
+    content: '2',
+    backgroundColor: '#32bf00',
+  },
+}
+
+export const Red: Story = {
+  args: {
+    content: '10',
+    backgroundColor: '#d4001a',
+  },
+}
